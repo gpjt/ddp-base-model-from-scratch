119527c5468eaa15c71a2699badd8bd8d82bd80f
Giles Thomas <giles@giles.net>
2025-12-03 17:59:16 +0000

    Copypasta of old big train code, adapted to use the runs stuff

---
+++ checkpointing.py
@@ -0,0 +1,73 @@
+import datetime
+import json
+from pathlib import Path
+
+from safetensors.torch import load_file, save_file
+import torch
+
+
+def get_checkpoints_dir(run_dir):
+    return run_dir / "checkpoints"
+
+
+def load_checkpoint(run_dir, checkpoint, model, optimizer=None, scaler=None):
+    checkpoints_dir = get_checkpoints_dir(run_dir)
+    checkpoint_dir = checkpoints_dir / checkpoint
+    model.load_state_dict(load_file(checkpoint_dir / "model.safetensors"))
+
+    if optimizer:
+        optimizer.load_state_dict(torch.load(checkpoint_dir / "optimizer.pt"))
+
+    if scaler:
+        scaler.load_state_dict(torch.load(checkpoint_dir / "scaler.pt"))
+
+    with open(checkpoint_dir / "meta.json", "r") as f:
+        meta = json.load(f)
+        restart_ds_offset = meta["train_ds_offset"] + 1
+
+    with open(checkpoints_dir / "best" / "meta.json") as f:
+        best_loss = json.load(f)["val_loss"]
+
+    return restart_ds_offset, best_loss
+
+
+def save_checkpoint(
+    run_dir,
+    name,
+    model, optimizer, scaler,
+    train_loss, val_loss,
+    train_ds_offset, is_best
+):
+    checkpoints_dir = get_checkpoints_dir(run_dir)
+    if not checkpoints_dir.exists():
+        checkpoints_dir.mkdir()
+
+    now = datetime.datetime.now(datetime.UTC)
+    checkpoint_name = f"{now:%Y%m%dZ%H%M%S}-{name}"
+    checkpoint_dir = checkpoints_dir / checkpoint_name
+    checkpoint_dir.mkdir()
+
+    save_file(model.state_dict(), checkpoint_dir / "model.safetensors")
+    torch.save(optimizer.state_dict(), checkpoint_dir / "optimizer.pt")
+    torch.save(scaler.state_dict(), checkpoint_dir / "scaler.pt")
+
+    with open(checkpoint_dir / "meta.json", "w") as f:
+        json.dump(
+            dict(
+                train_loss=train_loss,
+                val_loss=val_loss,
+                train_ds_offset=train_ds_offset,
+                is_best=is_best,
+            ),
+            f
+        )
+
+    symlink_target = Path(".") / checkpoint_dir.name
+    if is_best:
+        best_path = checkpoints_dir / "best"
+        best_path.unlink(missing_ok=True)
+        best_path.symlink_to(symlink_target, target_is_directory=True)
+
+    latest_path = checkpoints_dir / "latest"
+    latest_path.unlink(missing_ok=True)
+    latest_path.symlink_to(symlink_target, target_is_directory=True)
--- ddp_train.py
+++ ddp_train.py
@@ -3,32 +3,180 @@ import os
 from pathlib import Path
 
 import click
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+from safetensors.torch import load_file
 
 import torch
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data import Dataset
 
+from checkpointing import get_checkpoints_dir, save_checkpoint
 from gpt import GPTModel
 
 
-def train(model_conf):
-    torch.accelerator.set_device_index(int(os.environ["LOCAL_RANK"]))
-    acc = torch.accelerator.current_accelerator()
-    backend = torch.distributed.get_default_backend_for_device(acc)
-    dist.init_process_group(backend)
-    rank = dist.get_rank()
-    print(f"On rank {rank}.")
-    device_id = rank % torch.accelerator.device_count()
+BATCH_SIZE = 6
+SEQ_LENGTH = 1024
+VAL_AND_CHECKPOINT_INTERVAL = 2000
 
-    model = GPTModel(model_conf).to(device_id)
-    ddp_model = DDP(model, device_ids=[device_id])
+
+class BigTrainDataset(Dataset):
+
+    def __init__(self, all_tokens):
+        self.xs = all_tokens[:-1].reshape(-1, BATCH_SIZE, SEQ_LENGTH)
+        self.ys = all_tokens[1:].reshape(-1, BATCH_SIZE, SEQ_LENGTH)
+
+    def __getitem__(self, ix):
+        return (self.xs[ix], self.ys[ix])
+
+    def __len__(self):
+        return self.xs.shape[0]
+
+
+def load_dataset(run_dir, split):
+    return BigTrainDataset(
+        load_file(run_dir / "datasets" / f"{split}.safetensors")["tokens"]
+    )
+
+
+def get_training_data(run_dir):
+    checkpoints_dir = get_checkpoints_dir(run_dir)
+
+    train_losses = []
+    val_losses = []
+    best_train_ds_offset = None
+    for item in checkpoints_dir.iterdir():
+        if item.name == "latest":
+            continue
+
+        meta = json.loads((item / "meta.json").read_text())
+        if item.name == "best":
+            best_train_ds_offset = meta["train_ds_offset"]
+            continue
+
+        train_losses.append((meta["train_ds_offset"], meta["train_loss"]))
+        val_losses.append((meta["train_ds_offset"], meta["val_loss"]))
+
+    train_losses.sort(key=lambda x: x[0])
+    val_losses.sort(key=lambda x: x[0])
+
+    return train_losses, val_losses, best_train_ds_offset
+
+
+def generate_training_chart(run_dir):
+    train_points, val_points, best_train_ds_offset = get_training_data()
+
+    plt.title("TRAINING RUN LOSS")
+    plt.xkcd()
+    plt.rcParams['font.family'] = "xkcd"
+
+    fig, ax = plt.subplots(figsize=(8, 6), dpi=100)
+
+    train_epochs, train_losses = zip(*train_points)
+    val_epochs, val_losses = zip(*val_points)
+    ax.plot(train_epochs, train_losses, label="TRAINING LOSS", marker="o")
+    ax.plot(val_epochs, val_losses, label="VALIDATION LOSS", marker="s")
+
+    ax.axvline(
+        best_train_ds_offset, color="red", linestyle="--", linewidth=1.5,
+        label="BEST ITERATION"
+    )
+
+    ax.set_title("TRAINING RUN LOSS")
+    ax.set_xlabel("ITERATION")
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.set_ylabel("LOSS")
+    ax.legend()
+
+    fig.tight_layout()
+    image_file = run_dir / "big-training-run-chart.png"
+    fig.savefig(image_file, bbox_inches="tight")
+    plt.close(fig)
+
+
+def calculate_loss(logits, targets):
+    return torch.nn.functional.cross_entropy(
+        logits.flatten(0, 1), targets.flatten()
+    )
+
+
+def train(
+    run_dir,
+    model, optimizer, scaler,
+    train_ds, val_ds,
+    train_ds_offset, best_loss
+):
+    device = next(model.parameters()).device
+
+    torch.set_float32_matmul_precision("high")
+
+    print(f"Starting training at dataset offset {train_ds_offset}")
+    train_losses = []
+    for ix in tqdm(range(train_ds_offset, len(train_ds))):
+        model.train()
+        inputs, targets = train_ds[ix]
+        inputs = inputs.to(device).to(torch.long)
+        targets = targets.to(device).to(torch.long)
+
+        optimizer.zero_grad(set_to_none=True)
+
+        with torch.amp.autocast(device_type=device.type, dtype=torch.float16):
+            logits = model(inputs)
+
+            train_loss = calculate_loss(logits, targets)
+
+        scaler.scale(train_loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        train_losses.append(train_loss.item())
+
+        if (ix % VAL_AND_CHECKPOINT_INTERVAL == 0) or (ix == len(train_ds) - 1):
+            print("Validation/checkpoint")
+            model.eval()
+            with torch.inference_mode(), torch.amp.autocast(device_type=device.type, dtype=torch.float16):
+                val_losses = []
+                for val_inputs, val_targets in tqdm(val_ds):
+                    val_inputs = val_inputs.to(device).to(torch.long)
+                    val_targets = val_targets.to(device).to(torch.long)
+                    val_logits = model(val_inputs)
+                    val_losses.append(
+                        calculate_loss(val_logits, val_targets).item()
+                    )
+                val_loss = sum(val_losses) / len(val_losses)
+
+            if best_loss is None or val_loss < best_loss:
+                is_best = True
+                best_loss = val_loss
+            else:
+                is_best = False
+
+            avg_train_loss = sum(train_losses) / len(train_losses)
+            train_losses = []
+
+            save_checkpoint(
+                run_dir,
+                f"iteration-{ix}",
+                model, optimizer, scaler,
+                avg_train_loss, val_loss,
+                ix,
+                is_best
+            )
+            generate_training_chart(run_dir)
+
+            model.train()
+            print("Continuing training")
 
     dist.destroy_process_group()
 
 
 @click.command()
 @click.argument("run")
-def main(run):
+@click.argument("checkpoint", default=None)
+def main(run, checkpoint):
     run_dir = Path(__file__).resolve().parent / "runs" / run
     if not run_dir.is_dir():
         raise Exception(f"Could not find run dir {run_dir}")
@@ -39,7 +187,42 @@ def main(run):
     with open(model_conf_file, "r") as f:
         model_conf = json.load(f)
 
-    train(model_conf)
+    torch.accelerator.set_device_index(int(os.environ["LOCAL_RANK"]))
+    acc = torch.accelerator.current_accelerator()
+    backend = torch.distributed.get_default_backend_for_device(acc)
+    dist.init_process_group(backend)
+    rank = dist.get_rank()
+    print(f"On rank {rank}.")
+    device_id = rank % torch.accelerator.device_count()
+
+    model = GPTModel(model_conf).to(device_id)
+
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=0.0004, weight_decay=0.1
+    )
+
+    scaler = torch.amp.GradScaler()
+
+    train_ds = load_dataset(run_dir, "train")
+    val_ds = load_dataset(run_dir, "validation")
+
+    if checkpoint:
+        train_ds_offset, best_loss = load_checkpoint(
+            checkpoint, model, optimizer, scaler
+        )
+    else:
+        train_ds_offset = 0
+        best_loss = None
+
+    ddp_model = DDP(model, device_ids=[device_id])
+
+    train(
+        run_dir,
+        ddp_model, optimizer, scaler,
+        train_ds, val_ds,
+        train_ds_offset, best_loss
+    )
 
 
 if __name__ == "__main__":
