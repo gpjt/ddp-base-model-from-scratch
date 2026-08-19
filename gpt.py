9b3f03e45297774bfcb5d2cee6d7e5dcbf37143c
Giles Thomas <giles@giles.net>
2026-08-19 23:30:05 +0100

    Drop the hand-written GELU, which was annoying me.  That sped things up immensely!

--- gpt.py
+++ gpt.py
@@ -74,21 +74,13 @@ class MultiHeadAttention(nn.Module):
 
 
 
-
-class GELU(nn.Module):
-
-    def forward(self, x):
-        return 0.5 * x * (1 + torch.tanh(torch.sqrt(torch.tensor(2.0 / torch.pi)) * (x + 0.044715 * torch.pow(x, 3))))
-
-
-
 class FeedForward(nn.Module):
 
     def __init__(self, cfg):
         super().__init__()
         self.layers = nn.Sequential(
             nn.Linear(cfg["emb_dim"], cfg["emb_dim"] * 4),
-            GELU(),
+            nn.GELU(),
             nn.Linear(cfg["emb_dim"] * 4, cfg["emb_dim"])
         )
 
